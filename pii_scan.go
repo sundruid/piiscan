59092b453f9c1823a2a459b08387b70a4e8bda42
Sundruid <sundruid@users.noreply.github.com>
2025-02-13 14:32:27 -0800

    Update pii_scan.go

--- pii_scan.go
+++ pii_scan.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"bufio"
-	"io"
-	"math"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"math"
 	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
 	"sync"
-	"time"
 )
 
 var regexes = map[string]*regexp.Regexp{}
@@ -23,50 +22,59 @@ func init() {
 	regexes = map[string]*regexp.Regexp{
 		//"US address":            regexp.MustCompile(`\b\d{1,5}[\s\t,][A-Z][a-z]{0,25}[\s\t,][A-Z][a-z]{0,25}\b`),
 		//"EU address":            regexp.MustCompile(`\b[\w\s]+(?:\d[\w\s]*)?,\s[\w\s]+\b`),
-		"email address":           regexp.MustCompile(`\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b`),
-		"city/state/zip":          regexp.MustCompile(`\b[A-Z][a-z]{2,15}[\s\t,][A-Z]{2}[\s\t,]\d{5}\b`),
-		"international phone":     regexp.MustCompile(`\+\d{9,14}`),
-		"domestic phone":          regexp.MustCompile(`\b([2-9]\d{2}[-.\s]?)\d{3}[-.\s]?\d{4}\b`),
-		"possible birthdate":      regexp.MustCompile(fmt.Sprintf(`\b\d{1,2}[-.\s]\d{1,2}[-.\s](?:19\d{2}|[2-%d]\d{3})\b`, time.Now().Year()-18)),
-		"national/ssn id":         regexp.MustCompile(`\b\d{3}-\d{2}-\d{4}\b`),
-		"MC detected":             regexp.MustCompile(`\b(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}\b`),
-		"VISA detected":           regexp.MustCompile(`\b([4]\d{3}[\s]\d{4}[\s]\d{4}[\s]\d{4}|[4]\d{3}[-]\d{4}[-]\d{4}[-]\d{4}|[4]\d{3}[.]\d{4}[.]\d{4}[.]\d{4}|[4]\d{3}\d{4}\d{4}\d{4})\b`),
-		"AMEX detected":           regexp.MustCompile(`\b3[47][0-9]{13}\b`),
-		"sensitive_json_label":    regexp.MustCompile(`\b(nationalID|SSN)\b`),
-		"sensitive_sql_column":    regexp.MustCompile(`\b(nationalID|SSN)\b`),
+		"email address":        regexp.MustCompile(`(?i)[\w.+-]+@[\w-]+\.[\w.-]+`),
+		"city/state/zip":       regexp.MustCompile(`\b(?:[A-Z][a-z]{2,}(?:[\s-]+[A-Z][a-z]{2,})*[\s,]+[A-Z]{2}[\s,]+\d{5}(?:-\d{4})?)\b`),
+		"international phone":  regexp.MustCompile(`(?:\+|00)[1-9]\d{1,3}[\s.-]?\d{1,14}(?:\s*(?:x|ext\.?)\s*\d{1,5})?`),
+		"domestic phone":       regexp.MustCompile(`(?:\+?1[-.]?)?\s*\(?([0-9]{3})\)?[-.\s]?([0-9]{3})[-.\s]?([0-9]{4})`),
+		"possible birthdate":   regexp.MustCompile(fmt.Sprintf(`\b(?:\d{1,2}[-/]\d{1,2}[-/](?:19|20)\d{2}|(?:19|20)\d{2}[-/]\d{1,2}[-/]\d{1,2})\b`)),
+		"national/ssn id":      regexp.MustCompile(`\b\d{3}[-\s]?\d{2}[-\s]?\d{4}\b`),
+		"MC detected":          regexp.MustCompile(`\b(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}\b`),
+		"VISA detected":        regexp.MustCompile(`\b4[0-9]{12}(?:[0-9]{3})?(?:[0-9]{3})?\b`),
+		"AMEX detected":        regexp.MustCompile(`\b3[47][0-9]{13}\b`),
+		"DISCOVER detected":    regexp.MustCompile(`\b6(?:011|5[0-9]{2})[0-9]{12}\b`),
+		"JCB detected":         regexp.MustCompile(`\b(?:2131|1800|35\d{3})\d{11}\b`),
+		"sensitive_json_label": regexp.MustCompile(`\b(?i)(password|secret|api[_-]?key|auth[_-]?token|access[_-]?token|nationalID|SSN|credit[_-]?card)\b`),
+		"sensitive_sql_column": regexp.MustCompile(`\b(?i)(password|secret|api[_-]?key|auth[_-]?token|access[_-]?token|nationalID|SSN|credit[_-]?card)\b`),
 
 		// TLS private key regex
 		"TLS private key": regexp.MustCompile(`(?s).*?-----BEGIN (RSA PRIVATE|EC PRIVATE|PRIVATE) KEY-----.*?-----END (RSA PRIVATE|EC PRIVATE|PRIVATE) KEY-----`),
 
+		// New patterns
+		"passport number": regexp.MustCompile(`\b[A-Z]\d{7}\b`), // US passport format
+		"drivers license": regexp.MustCompile(`\b[A-Z]\d{7}\b`), // Common format, adjust per state
+		"ip address":      regexp.MustCompile(`\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b`),
+		"mac address":     regexp.MustCompile(`\b([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\b`),
+		"aws key":         regexp.MustCompile(`(?i)AKIA[0-9A-Z]{16}`),
+		"bitcoin address": regexp.MustCompile(`\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b`),
+		"iban":            regexp.MustCompile(`\b[A-Z]{2}\d{2}[A-Z0-9]{4}\d{7}([A-Z0-9]?){0,16}\b`),
 	}
 }
 
 func isTextFile(file string) bool {
-    f, err := os.Open(file)
-    if err != nil {
-        return false
-    }
-    defer f.Close()
+	f, err := os.Open(file)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
 
-    fileInfo, err := f.Stat()
-    if err != nil {
-        return false
-    }
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return false
+	}
 
-    // Get the size of the file and adjust the buffer size accordingly
-    fileSize := fileInfo.Size()
-    bufferSize := int(math.Min(float64(fileSize), 512))
+	// Get the size of the file and adjust the buffer size accordingly
+	fileSize := fileInfo.Size()
+	bufferSize := int(math.Min(float64(fileSize), 512))
 
-    buf := make([]byte, bufferSize)
-    _, err = f.Read(buf)
-    if err != nil && err != io.EOF {
-        return false
-    }
+	buf := make([]byte, bufferSize)
+	_, err = f.Read(buf)
+	if err != nil && err != io.EOF {
+		return false
+	}
 
-    contentType := http.DetectContentType(buf)
-    return strings.HasPrefix(contentType, "text")
+	contentType := http.DetectContentType(buf)
+	return strings.HasPrefix(contentType, "text")
 }
-
 
 func isSQL(file string) bool {
 	return strings.HasSuffix(file, ".sql")
@@ -232,12 +240,32 @@ func scanTextFile(file string) {
 	for typeName, re := range regexes {
 		matches := re.FindAllString(string(data), -1)
 		if len(matches) > 0 {
-			fmt.Printf("In file %s, found %d instances of %s. Sample matches:\n", file, len(matches), typeName)
-			for i, match := range matches {
-				if i >= maxSamplesPerType {
-					break // Limit the number of samples printed
+			validMatches := matches[:0]
+
+			// Validate matches based on type
+			for _, match := range matches {
+				isValid := true
+
+				switch typeName {
+				case "MC detected", "VISA detected", "AMEX detected", "DISCOVER detected", "JCB detected":
+					isValid = validateCreditCard(match)
+					// Add more validation cases as needed
 				}
-				fmt.Printf("  Match %d: %s\n", i+1, match)
+
+				if isValid {
+					validMatches = append(validMatches, match)
+				}
+			}
+
+			if len(validMatches) > 0 {
+				fmt.Printf("In file %s, found %d valid instances of %s. Sample matches:\n",
+					file, len(validMatches), typeName)
+				for i, match := range validMatches {
+					if i >= maxSamplesPerType {
+						break
+					}
+					fmt.Printf("  Match %d: %s\n", i+1, match)
+				}
 			}
 		}
 	}
@@ -298,9 +326,30 @@ func checkForObfuscationTag(file string) bool {
 	return strings.Contains(string(data), obfuscationUUID)
 }
 
+// Add validation functions for detected patterns
+func validateCreditCard(number string) bool {
+	// Luhn algorithm implementation
+	var sum int
+	var alternate bool
+
+	numStr := strings.ReplaceAll(strings.ReplaceAll(number, "-", ""), " ", "")
+	for i := len(numStr) - 1; i >= 0; i-- {
+		n := int(numStr[i] - '0')
+		if alternate {
+			n *= 2
+			if n > 9 {
+				n = (n % 10) + 1
+			}
+		}
+		sum += n
+		alternate = !alternate
+	}
+	return sum%10 == 0
+}
+
 func main() {
 
-	fmt.Println("pii_scanner v.3 maintained by kenneth.webster@imperva.com")
+	fmt.Println("pii_scanner v.4 maintained by kenneth.webster@imperva.com")
 
 	filesystem := flag.String("filesystem", "", "The root of the filesystem to scan")
 	flag.Parse()
